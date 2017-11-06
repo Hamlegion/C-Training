23526a5d17af33b1c6a8db874e00364f4863ca3a
Hamlegion <stefan.dinescu92@gmail.com>
2017-11-06 14:27:19 +0200

    First Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/home")]
+    public class HomeController : ApiController
+    {
+        // GET: api/Home
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<string> GetBasicInfo()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Home/5
+        [HttpGet]
+        [Route("{accountId}")]
+        public string Get(int accountId)
+        {
+            return "value";
+        }
+
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Home/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Home/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
